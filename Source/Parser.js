2d54d70f7dddcebb1416a6424fdf50e71595a3f3
Ben Emmons <ben@emmons.biz>
2018-10-26 03:53:14 +0100

    Updated classification to allow easier scaling.

--- Source/Parser.js
+++ Source/Parser.js
@@ -7,23 +7,33 @@ module.exports = class SourceFile {
 
     parse(line) {
         let lineProperties = {};
-        let firstWord = line.split(" ")[0];
 
-        if (["let", "var", "const"].includes(firstWord)) {
+        let classifiers = {
+            "declaration": ["let", "var", "const"].includes(line.split(" ")[0]),
+            "addition": line.includes("+"),
+            "subtraction": line.includes("-")
+        };
+
+        if (classifiers.declaration) {
             lineProperties.intent = "declaration";
             lineProperties.variableName = line.split(":")[0].split(" ")[1];
             lineProperties.value = line.split(":")[1].split("=")[1];
-            lineProperties.valueType = line.split(":")[1].split("=")[0].trim();
+            lineProperties.valueType = line.split(":")[1].split("=")[0];
 
-            for (let key in lineProperties) { // Trims every non-undefined value.
-                let value = lineProperties[key];
-                if (value != undefined) {
-                    lineProperties[key] = value.trim();
-                }
+        } else if (classifiers.addition) {
+            lineProperties.intent = "addition";
+            lineProperties.variableName = line.split("=")[0];
+            lineProperties.firstValue = line.split("=")[1].split("+")[0];
+            lineProperties.secondValue = line.split("=")[1].split("+")[1];
 
-            }
         } else {
             lineProperties.intent = "other";
+        }
+
+        for (let key in lineProperties) { // Trims every non-undefined value.
+            if (lineProperties[key] != undefined) {
+                lineProperties[key] = lineProperties[key].trim();
+            }
         }
         return lineProperties;
     }
